a0266d5b2d7babc91247bc4a3583e4186382ec43
richiealy <richard36098@hotmail.com>
2016-09-07 16:09:43 +1000

    sprint 1 post demo

--- unittests.js
+++ unittests.js
@@ -6,3 +6,18 @@ QUnit.test("toggle report", function(assert)
 	toggle_sales_report();
     assert.equal(salesreport.isVisible(), true, "Report should be visible.");
 });
+
+//testing toggle_edit_sales_report
+QUnit.test("toggle edit sales", function(assert)
+{
+assert.equal(salesreport.isVisible(), true, "Edit Sales Report should be visible.");
+toggle_edit_sales_report();
+});
+		
+//testing toggle_add_sales_report
+QUnit.test("toggle add sales", function(assert)
+{
+toggle_add_sales_report();
+assert.equal(addsalesreport.isVisible(), true, "Add Sales Report should be visible.");
+});
+		
